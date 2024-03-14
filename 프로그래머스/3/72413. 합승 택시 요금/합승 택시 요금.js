636a4bf5ae8152bfc81b0760fceb670710d37a8d
Inhwa Kim <radiantbeing99@gmail.com>
2024-03-14 17:08:03 +0900

    [level 3] Title: 합승 택시 요금, Time: 13.27 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
@@ -0,0 +1,28 @@
+function solution(n, s, a, b, fares) {
+    const graph = Array.from({ length: n + 1 }, () => Array(n + 1).fill(Infinity));
+    
+    for (let i = 0; i < n + 1; i++) {
+        graph[i][i] = 0;
+    }
+    
+    for (const [v1, v2, cost] of fares) {
+        graph[v1][v2] = cost;
+        graph[v2][v1] = cost;
+    }
+    
+    for (let k = 1; k < n + 1; k++) {
+        for (let i = 1; i < n + 1; i++) {
+            for (let j = 1; j < n + 1; j++) {
+                graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+            }
+        }
+    }
+    
+    let answer = Infinity;
+    
+    for (let k = 1; k < n + 1; k++) {
+        answer = Math.min(answer, graph[s][k] + graph[k][a] + graph[k][b]);    
+    }
+    
+    return answer;
+}
