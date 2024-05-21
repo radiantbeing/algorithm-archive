95993a98b6f33e5ef87a705823a14309bf0b4bad
Inhwa Kim <radiantbeing99@gmail.com>
2024-05-21 11:19:32 +0900

    [Silver I] Title: IOIOI, Time: 196 ms, Memory: 12120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -14,20 +14,24 @@ const solve = () => {
     const M = Number(data[1]);
     const S = data[2];
 
-    const getP = () => {
-        let str = "I";
-        for (let i = 0; i < N; i++) {
-            str += "OI";
-        }
-        return str;
-    };
-
-    const P = getP();
     let answer = 0;
+    let count = 0;
+    let i = 0;
 
-    for (let i = 0; i < M; i++) {
-        if (S.substring(i, i + P.length) === P) {
-            answer++;
+    while (i < M - 1) {
+        if (S[i] === 'I' && S[i + 1] === 'O') {
+            let j = i + 1;
+            while (j < M - 1 && S[j] === 'O' && S[j + 1] === 'I') {
+                j += 2;
+                count++;
+                if (count >= N) {
+                    answer++;
+                }
+            }
+            i = j;
+            count = 0;
+        } else {
+            i++;
         }
     }
 
