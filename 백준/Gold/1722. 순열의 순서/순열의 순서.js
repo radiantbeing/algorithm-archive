efe75702e6bd067b3c1ab433641fa0d809e82ebd
Inhwa Kim <radiantbeing99@gmail.com>
2024-06-16 15:23:57 +0900

    [Gold V] Title: 순열의 순서, Time: 120 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Gold/1722. 순열의 순서/순열의 순서.js
+++ 백준/Gold/1722. 순열의 순서/순열의 순서.js
@@ -1,62 +1,61 @@
 const fs = require("fs");
 
 const data = fs
-    .readFileSync(
-        process.platform === "linux" ? 0 : "input.txt",
-        "utf-8"
-    )
-    .toString()
-    .trimEnd()
-    .split("\n");
+  .readFileSync(
+    process.platform === "linux" ? 0 : "input.txt",
+    "utf-8"
+  )
+  .toString()
+  .trim()
+  .split("\n");
 
-    
 const solve = () => {
-    const N = Number(data[0]);
-    const F = Array(21).fill(BigInt(0));
-    const S = Array(21).fill(BigInt(0));
-    const visited = Array(21).fill(false);
-   
-    F[0] = BigInt(1);
+  const N = Number(data[0]);
+  const [problem, ...rest] = data[1].split(" ");
+  const factorial = Array(N + 1).fill(BigInt(1));
+  const isVisited = Array(N + 1).fill(false);
+  const sequence = Array(N + 1);
 
+  for (let i = 1; i < N + 1; i++) {
+    factorial[i] = factorial[i - 1] * BigInt(i);
+  }
+  
+  let answer;
+
+  if (problem === "1") {
+    let K = BigInt(rest[0]);
     for (let i = 1; i < N + 1; i++) {
-        F[i] = F[i - 1] * BigInt(i);
+      let count = BigInt(1);
+      for (let j = 1; j < N + 1; j++) {
+        if (isVisited[j]) {
+          continue;
+        }
+        if (K <= factorial[N - i] * count) {
+          K -= factorial[N - i] * (count - BigInt(1));
+          sequence[i] = j;
+          isVisited[j] = true;
+          break;
+        }
+        count++;
+      }
     }
-
-    const line = data[1].split(" ").map(BigInt);
-
-    if (line[0] === BigInt(1)) {
-        let K = BigInt(line[1]);
-        for (let i = 1; i < N + 1; i++) {
-            let count = BigInt(1);
-            for (j = 1; j < N + 1; j++) {
-                if (visited[j]) continue;
-                if (K <= count * F[N - i]) {
-                    K -= (count - BigInt(1)) * F[N - i];
-                    S[i] = j;
-                    visited[j] = true;
-                    break;
-                }
-                count++;
-            }
+    answer = sequence.slice(1).join(" ");
+  } else if (problem === "2") {
+    const permutation = rest.map(BigInt);
+    let K = BigInt(1);
+    for (let i = 1; i < N + 1; i++) {
+      let count = BigInt(0);
+      for (let j = 1; j < permutation[i - 1]; j++) {
+        if (!isVisited[j]) {
+          count++;
         }
-        let answer = "";
-        for (let i = 1; i < N + 1; i++) {
-            answer += `${S[i]} `;
-        }
-        return answer;
-    } else if (line[0] === BigInt(2)) {
-        let K = BigInt(1);
-        for (let i = 1; i < N + 1; i++) {
-            let count = BigInt(0);
-            for (let j = 1; j < line[i]; j++) {
-                if (!visited[j])
-                    count += BigInt(1);
-            }
-            K += count * F[N - i];
-            visited[line[i]] = true;
-        }
-        return K.toString();
+      }
+      K += factorial[N - i] * count;
+      isVisited[permutation[i - 1]] = true;
     }
+    answer = K.toString();
+  }
+  return answer;
 };
 
 console.log(solve());
