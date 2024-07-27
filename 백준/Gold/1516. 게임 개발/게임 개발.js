ec1e75d21b4814118948926b174ca4137fdbdb20
Inhwa Kim <radiantbeing99@gmail.com>
2024-07-27 19:28:30 +0900

    [Gold III] Title: 게임 개발, Time: 192 ms, Memory: 15628 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.js
+++ 백준/Gold/1516. 게임 개발/게임 개발.js
@@ -1,69 +1,71 @@
 const fs = require("fs");
 
-class LineReader {
-    data = fs
-        .readFileSync(
-            process.platform === "linux" ? 0 : "input.txt",
-            "utf-8"
-        )
-        .toString()
-        .trim()
-        .split("\n");
-    cursor = 0;
+class Reader {
+  data = fs
+    .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf-8")
+    .toString()
+    .trim()
+    .split("\n");
+  
+  cursor = 0;
 
-    read() {
-        return this.data[this.cursor++];
-    }
+  read() {
+    return this.data[this.cursor++];
+  }
 
-    readInt() {
-        return Number(this.read());
-    }
+  readInt() {
+    return Number(this.read());
+  }
 
-    readIntArray() {
-        return this.read().split(" ").map(Number);
-    }
+  readIntArray() {
+    return this.read().split(" ").map(Number);
+  }
 }
 
-const solve = () => {
-    const lineReader = new LineReader();
+function solve() {
+  const reader = new Reader();
+  const N = reader.readInt();
+  const graph = Array.from({ length: N + 1 }, () => []);
+  const selfBuild = Array(N + 1).fill(0);
 
-    const N = lineReader.readInt();
-    const graph = Array.from({ length: N + 1 }, () => []);
-    const inDegree = Array(N + 1).fill(0);
-    const build = Array(N + 1).fill(0);
+  const indegree = Array(N + 1).fill(0);
 
-    for (let now = 1; now < N + 1; now++) {
-        const [time, ...pres] = lineReader.readIntArray();
-        build[now] = time;
-        for (const pre of pres) {
-            if (pre === -1) break;
-            inDegree[now]++;
-            graph[pre].push(now);
-        }
+  for (let i = 1; i <= N; i++) {
+    const [time, ...preBuilds] = reader.readIntArray();
+
+    selfBuild[i] = time;
+
+    for (const build of preBuilds) {
+      if (build === -1) break;
+      graph[build].push(i);
+      indegree[i]++;
     }
+  }
 
-    const queue = [];
-    const answer = Array(N + 1).fill(0);
+  const queue = [];
 
-    for (let now = 1; now < N + 1; now++) {
-        if (inDegree[now] === 0) {
-            queue.push(now);
-            answer[now] = build[now];
-        } 
+  for (let i = 1; i <= N; i++) {
+    if (indegree[i] === 0)
+      queue.push(i);
+  }
+
+  const answer = Array(N + 1).fill(0);
+
+  while (queue.length > 0) {
+    const now = queue.shift();
+    for (const next of graph[now]) {
+      indegree[next]--;
+      answer[next] = Math.max(answer[next], answer[now] + selfBuild[now]);
+      if (indegree[next] === 0)
+        queue.push(next);
     }
+  }
 
-    while (queue.length > 0) {
-        const now = queue.shift();
-        for (const next of graph[now]) {
-            inDegree[next]--;
-            answer[next] = Math.max(answer[next], answer[now] + build[next]);
-            if (inDegree[next] === 0) {
-                queue.push(next);
-            }
-        }
-    }
+  for (let i = 1; i <= N; i++) {
+    answer[i] += selfBuild[i];
+  }
 
-    return answer.slice(1).join("\n");
-};
+  return answer.slice(1).join("\n");
+}
 
 console.log(solve());
