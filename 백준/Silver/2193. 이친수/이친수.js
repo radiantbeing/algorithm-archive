a7b08a1510ad8c4f749dd97f55e03d78bb438cbb
Inhwa Kim <radiantbeing99@gmail.com>
2024-08-02 14:50:59 +0900

    [Silver III] Title: 이친수, Time: 100 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.js
+++ 백준/Silver/2193. 이친수/이친수.js
@@ -1,27 +1,24 @@
 const fs = require("fs");
 
 const data = fs
-  .readFileSync(
-    process.platform === "linux" ? 0 : "input.txt",
-    "utf-8"
-  )
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf-8")
   .toString()
   .trim();
 
-const solve = () => {
-  const N = Number(data);
+function solve() {
+  const N = parseInt(data);
 
-  const dp = Array.from({ length: 2 }, () => Array(N + 1));
-  
-  dp[0][1] = BigInt(0);
+  const dp = Array.from({ length: N + 1 }, () => new Array(2));
+
+  dp[1][0] = BigInt(0);
   dp[1][1] = BigInt(1);
 
-  for (let i = 2; i < N + 1; i++) {
-    dp[0][i] = dp[0][i - 1] + dp[1][i - 1];
-    dp[1][i] = dp[0][i - 1];
+  for (let i = 2; i <= N; i++) {
+    dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+    dp[i][1] = dp[i - 1][0];
   }
 
-  return String(dp[0][N] + dp[1][N]);
-};
+  return (dp[N][0] + dp[N][1]).toString();
+}
 
 console.log(solve());
