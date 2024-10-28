a464db8ac3abe61dd452ce1126f9f26a140b3a4d
김인화 <radiantbeing99@gmail.com>
2024-10-28 23:17:35 +0900

    [Gold IV] Title: 플로이드, Time: 288 ms, Memory: 27116 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.js
+++ 백준/Gold/11404. 플로이드/플로이드.js
@@ -1,66 +1,61 @@
 const fs = require("fs");
 
-class Reader {
-  data = fs
+const reader = {
+  data: fs
     .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf-8")
     .toString()
     .trim()
-    .split("\n");
+    .split("\n"),
   
-  cursor = 0;
+  cursor: 0,
 
   read() {
     return this.data[this.cursor++];
-  }
+  },
 
   readInt() {
-    return Number(this.read());
-  }
+    return parseInt(this.read());
+  },
 
   readIntArray() {
-    return this.read().split(" ").map(Number);
+    return this.read().split(" ").map(s => parseInt(s));
   }
-}
+};
 
 function solve() {
-  const reader = new Reader();
   const N = reader.readInt();
   const M = reader.readInt();
 
-  // 인접 행렬
-  const graph = Array.from(
-    { length: N + 1 }, 
-    () => new Array(N + 1).fill(Infinity)
-  );
+  const distance = Array.from({ length: N + 1 }, () => new Array(N + 1).fill(Infinity));
 
   for (let i = 1; i <= N; i++) {
-    graph[i][i] = 0;
+    distance[i][i] = 0;
   }
-  
+
   for (let i = 0; i < M; i++) {
-    const [s, e, w] = reader.readIntArray();
-    graph[s][e] = Math.min(graph[s][e], w);
+    const [A, B, C] = reader.readIntArray();
+    distance[A][B] = Math.min(distance[A][B], C);
   }
 
   for (let k = 1; k <= N; k++) {
     for (let i = 1; i <= N; i++) {
       for (let j = 1; j <= N; j++) {
-        graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+        distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
       }
     }
   }
 
-  const answer = [];
+  let answer = "";
 
   for (let i = 1; i <= N; i++) {
-    const temp = [];
     for (let j = 1; j <= N; j++) {
-      temp.push(graph[i][j] === Infinity ? 0 : graph[i][j]);
+      const dist = distance[i][j] === Infinity ? 0 : distance[i][j];
+      answer += `${dist} `;
     }
-    answer.push(temp.join(" "));
+    answer += "\n";
   }
 
-  return answer.join("\n");
+  return answer;
 }
 
 console.log(solve());
