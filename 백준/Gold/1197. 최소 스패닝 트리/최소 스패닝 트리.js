566c51390a770e0be81ac93797f1e619cce30a66
김인화 <radiantbeing99@gmail.com>
2024-10-31 22:00:52 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 416 ms, Memory: 48368 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.js
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.js
@@ -1,53 +1,61 @@
-'use strict'
+const fs = require("fs");
 
-const fs = require('fs');
+class LineReader {
+  data = fs
+    .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf-8")
+    .toString()
+    .trim()
+    .split("\n");
+  cursor = 0;
 
-const input = fs.readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input').toString().trim().split('\n');
-const [n, m] = input.shift().split(' ').map(Number);
-const edges = input.map((i) => i.split(' ').map(Number));
+  read() {
+    return this.data[this.cursor++];
+  }
 
-function solution(n, m, edges) {
-    let answer = 0;
-
-    const parent = Array(n + 1).fill(0).map((_, index) => index);
-
-    function find(v) {
-        if (parent[v] === v) {
-            return v;
-        }
-
-        parent[v] = find(parent[v]);
-        return parent[v];
-    }
-
-    function union(u, v) {
-        const U = find(u);
-        const V = find(v);
-
-        parent[V] = U;
-    }
-
-    edges.sort((a, b) => a[2] - b[2]);
-
-    for (let [start, end, weight] of edges) {
-        const node1 = find(start);
-        const node2 = find(end);
-
-        if (node1 === node2) {
-            continue;
-        }
-
-        union(node1, node2);
-        answer += weight;
-        
-        if (answer === n - 1) {
-            break;
-        }
-    }
-
-    return answer;
+  readIntArray() {
+    return this.read().split(" ").map(Number);
+  }
 }
 
-const answer = solution(n, m, edges);
+const solve = () => {
+  const lineReader = new LineReader();
 
-console.log(answer);
+  const [V, E] = lineReader.readIntArray();
+  const edges = [];
+
+  for (let i = 0; i < E; i++) {
+    edges.push(lineReader.readIntArray());
+  }
+
+  const parents = Array.from({ length: V + 1 }, (_, i) => i);
+
+  const find = (v) => {
+    if (parents[v] === v) return v;
+    return (parents[v] = find(parents[v]));
+  };
+
+  const union = (u, v) => {
+    parents[find(u)] = find(v);
+  };
+
+  let count = 0;
+  let sum = 0;
+
+  edges.sort((a, b) => a[2] - b[2]);
+    
+  for (const [start, end, weight] of edges) {
+    if (find(start) === find(end)) {
+      continue;
+    }
+    union(start, end);
+    count++;
+    sum += weight;
+    if (count === V - 1) {
+      break;
+    }
+  }
+
+  return sum;
+};
+
+console.log(solve());
