ec5c595f230b3fb01671274b8b4b5ece1e92cc42
김인화 <radiantbeing99@gmail.com>
2024-11-03 22:22:05 +0900

    [Platinum V] Title: LCA 2, Time: 752 ms, Memory: 112472 KB -BaekjoonHub

--- 백준/Platinum/11438. LCA 2/LCA 2.js
+++ 백준/Platinum/11438. LCA 2/LCA 2.js
@@ -1,38 +1,35 @@
 const fs = require("fs");
 
-class LineReader {
-  data = fs
-    .readFileSync(
-      process.platform === "linux" ? 0 : "input.txt",
-      "utf-8"
-    )
+const reader = {
+  data: fs
+    .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf-8")
     .toString()
     .trim()
-    .split("\n");
-  cursor = 0;
+    .split("\n"),
+  
+  cursor: 0,
 
   read() {
     return this.data[this.cursor++];
-  }
+  },
 
   readInt() {
-    return Number(this.read());
-  }
+    return parseInt(this.read());
+  },
 
   readIntArray() {
-    return this.read().split(" ").map(Number);
+    return this.read().split(" ").map(s => parseInt(s));
   }
-}
+};
 
-const solve = () => {
-  const lineReader = new LineReader();
-  const N = lineReader.readInt();
-  const graph = Array.from({ length: N + 1 }, () => []);
+function solve() {
+  const N = reader.readInt();
+  const tree = Array.from({ length: N + 1 }, () => []);
 
   for (let i = 0; i < N - 1; i++) {
-    const [u, v] = lineReader.readIntArray();
-    graph[u].push(v);
-    graph[v].push(u);
+    const [node1, node2] = reader.readIntArray();
+    tree[node1].push(node2);
+    tree[node2].push(node1); 
   }
 
   let kMax = 0;
@@ -40,70 +37,68 @@ const solve = () => {
   while (2 ** kMax <= N) {
     kMax++;
   }
-  
-  const depth = Array(N + 1).fill(0);
-  const visited = Array(N + 1).fill(false);
-  const parent = Array.from({ length: kMax + 1 }, () => Array(N + 1).fill(0));
 
-  const bfs = () => {
+  const depth = Array(N + 1).fill(0);
+  const parent = Array.from({ length: kMax + 1 }, () => Array(N + 1).fill(0));
+  const visited = Array(N + 1).fill(false);
+
+  const bfs = (node) => {
     const queue = [];
-    queue.push(1);
-    visited[1] = true;
+    queue.push(node);
+    visited[node] = true;
+    
     while (queue.length > 0) {
       const now = queue.shift();
-      for (const next of graph[now]) {
+      for (const next of tree[now]) {
         if (!visited[next]) {
           visited[next] = true;
-          depth[next] = depth[now] + 1;
-          parent[0][next] = now;
           queue.push(next);
+          parent[0][next] = now;
+          depth[next] = depth[now] + 1;
         }
       }
     }
   };
+ 
+  bfs(1);
 
-  bfs();
-  
   for (let k = 1; k <= kMax; k++) {
-    for (let i = 0; i < N + 1; i++) {
-      parent[k][i] = parent[k - 1][parent[k - 1][i]]
+    for (let n = 1; n <= N; n++) {
+      parent[k][n] = parent[k - 1][parent[k - 1][n]];
     }
   }
 
-  const getLCA = (u, v) => {
-    if (depth[u] > depth[v]) {
-      [u, v] = [v, u];
-    }
-  
+  const getLCA = (a, b) => {
+    if (depth[a] > depth[b])
+      [a, b] = [b, a];
+
     for (let k = kMax; k >= 0; k--) {
-      if (depth[v] - depth[u] >= (1 << k)) {
-        v = parent[k][v];
+      if (1 << k <= depth[b] - depth[a])
+        b = parent[k][b];
+    }
+
+    if (a === b)
+      return a;
+
+    for (let k = kMax; k >= 0; k--) {
+      if (parent[k][a] !== parent[k][b]) {
+        a = parent[k][a];
+        b = parent[k][b];
       }
     }
-  
-    if (u === v) return u;
-  
-    for (let k = kMax; k >= 0; k--) {
-      if (parent[k][u] !== parent[k][v]) {
-        u = parent[k][u];
-        v = parent[k][v];
-      }
-    }
-  
-    return parent[0][u];
+
+    return parent[0][a];
   };
 
-  const M = lineReader.readInt();
-
+  const M = reader.readInt();
   let answer = "";
 
   for (let i = 0; i < M; i++) {
-    const [u, v] = lineReader.readIntArray();
-    const LCA = getLCA(u, v);
-    answer += `${LCA}\n`;
+    const [node1, node2] = reader.readIntArray();
+    answer += `${getLCA(node1, node2)}\n`;
   }
 
   return answer;
-};
+}
 
 console.log(solve());
