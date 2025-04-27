ca46b52293d8b88336af5ad5d7ee8ad4320b0b3f
김인화 <radiantbeing99@gmail.com>
2025-04-27 16:33:10 +0900

    [Gold III] Title: 게임 개발, Time: 180 ms, Memory: 15144 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.js
+++ 백준/Gold/1516. 게임 개발/게임 개발.js
@@ -1,71 +1,77 @@
 const fs = require("fs");
 
-class Reader {
-  data = fs
+const reader = (function () {
+  const lines = fs
     .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf-8")
-    .toString()
-    .trim()
+    .trimEnd()
     .split("\n");
-  
-  cursor = 0;
 
-  read() {
-    return this.data[this.cursor++];
+  let cursor = 0;
+
+  function get_line() {
+    return lines[cursor++];
   }
 
-  readInt() {
-    return Number(this.read());
+  function get_integer() {
+    return parseInt(get_line());
   }
 
-  readIntArray() {
-    return this.read().split(" ").map(Number);
+  function get_integers() {
+    return get_line()
+      .split(" ")
+      .map((token) => parseInt(token));
   }
-}
+
+  return {
+    get_integer,
+    get_integers,
+  };
+})();
 
 function solve() {
-  const reader = new Reader();
-  const N = reader.readInt();
-  const graph = Array.from({ length: N + 1 }, () => []);
-  const selfBuild = Array(N + 1).fill(0);
+  const N = reader.get_integer(); // 건물의 종류 수
+  const times = Array(N + 1).fill(0); // 건물을 짓는데 걸리는 시간
+  const total_times = Array(N + 1).fill(0); // 건물을 짓기 위한 총 시간
+  const graph = Array.from({ length: N + 1 }, () => []); // 인접 리스트
+  const indegrees = Array(N + 1).fill(0); // 진입 차수
 
-  const indegree = Array(N + 1).fill(0);
-
-  for (let i = 1; i <= N; i++) {
-    const [time, ...preBuilds] = reader.readIntArray();
-
-    selfBuild[i] = time;
-
-    for (const build of preBuilds) {
-      if (build === -1) break;
-      graph[build].push(i);
-      indegree[i]++;
-    }
+  for (let building_nr = 1; building_nr <= N; building_nr++) {
+    const [time, ...building_nrs] = reader.get_integers();
+    times[building_nr] = time;
+    building_nrs.forEach((pre_building_nr) => {
+      if (pre_building_nr > 0) {
+        graph[pre_building_nr].push(building_nr);
+        indegrees[building_nr]++;
+      }
+    });
   }
 
   const queue = [];
 
-  for (let i = 1; i <= N; i++) {
-    if (indegree[i] === 0)
-      queue.push(i);
-  }
-
-  const answer = Array(N + 1).fill(0);
-
-  while (queue.length > 0) {
-    const now = queue.shift();
-    for (const next of graph[now]) {
-      indegree[next]--;
-      answer[next] = Math.max(answer[next], answer[now] + selfBuild[now]);
-      if (indegree[next] === 0)
-        queue.push(next);
+  for (let building_nr = 1; building_nr <= N; building_nr++) {
+    if (indegrees[building_nr] === 0) {
+      queue.push(building_nr);
     }
   }
 
-  for (let i = 1; i <= N; i++) {
-    answer[i] += selfBuild[i];
+  while (queue.length > 0) {
+    const building_nr = queue.shift();
+    for (const next_building_nr of graph[building_nr]) {
+      total_times[next_building_nr] = Math.max(
+        total_times[next_building_nr],
+        total_times[building_nr] + times[building_nr],
+      );
+      indegrees[next_building_nr]--;
+      if (indegrees[next_building_nr] === 0) {
+        queue.push(next_building_nr);
+      }
+    }
   }
 
-  return answer.slice(1).join("\n");
+  return total_times
+    .map((total_time, building_nr) => total_time + times[building_nr])
+    .slice(1)
+    .join("\n");
 }
 
 console.log(solve());
